948e74f9e602dc8ad34f3fc2b65a4375c640f61a
Sirus01 <97009471+Sirus01@users.noreply.github.com>
2023-11-29 13:24:39 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,26 @@
 finch.startFinch()
 let isAngry = false
+let distance = 0
 
 basic.forever(function(){
     finch.setBeak(255, 255, 255)
-    loops.everyInterval(1, function(){
-        let distance = finch.getDistance()
-        if (distance <= 100) {
-            isAngry = true
-        } else {
-            isAngry = false
-        }
-    })
+})
+loops.everyInterval(1, function () {
+    distance = finch.getDistance()
+    if (distance <= 100) {
+        isAngry = true
+    } else {
+        isAngry = false
+    }
 })
 
 loops.everyInterval(1, function(){
+    distance = finch.getDistance()
     if (isAngry) {
-        let distance1 = finch.getDistance()
         basic.showIcon(IconNames.Angry)
-        finch.setMove(MoveDir.Forward, distance1 * 1.25, 100)
+        finch.setMove(MoveDir.Forward, distance * 2.75, 100)
     } else {
         basic.showIcon(IconNames.Asleep)
         finch.setTurn(RLDir.Right, 15, 100)
     }
-})
-
-input.onButtonPressed(Button.A, function(){
-    isAngry = true
-})
-input.onButtonPressed(Button.B, function () {
-    isAngry = false
 })
